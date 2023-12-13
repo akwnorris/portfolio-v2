084eebe0a3f920bd313d81c22d8c9f9668743062
Alex Norris <akwnorris@gmail.com>
2023-12-13 12:11:49 -0600

    Refactored smooth scrolling for navigation links

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+  anchor.addEventListener('click', function(e) {
       e.preventDefault();
-  
+      
       const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
+      const sectionId = this.getAttribute('href');
+      
+      switch (sectionId) {
+          case '#about':
+              document.getElementById('hero').scrollIntoView({ behavior: 'smooth' });
+              break;
+          case '#contact':
+              document.querySelector('.page-footer').scrollIntoView({ behavior: 'smooth' });
+              break;
+          default:
+              target.scrollIntoView({ behavior: 'smooth' });
+              break;
+      }
   });
-  
+});
